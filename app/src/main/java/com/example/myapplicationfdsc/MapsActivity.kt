312f12adef05c725f8f2f856836b39ad744ee930
burak <buraksoftware1@gmail.com>
2023-03-10 21:11:35 +0300

    Added device location funcs

--- app/src/main/java/com/example/myapplicationfdsc/MapsActivity.kt
+++ app/src/main/java/com/example/myapplicationfdsc/MapsActivity.kt
@@ -1,7 +1,17 @@
 package com.example.myapplicationfdsc
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,6 +25,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,21 +40,57 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+   /*     val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15f))*/
+
+
+        locationManager = getSystemService (Context.LOCATION_SERVICE) as LocationManager
+
+        locationListener = object : LocationListener{
+            override fun onLocationChanged(p0: Location) {
+                // konum değişirse yapılacak işlemler
+               // println(p0.latitude)
+               // println(p0.longitude)
+
+                val guncelKonum = LatLng (p0.latitude, p0.longitude)
+                mMap.addMarker (MarkerOptions() .position (guncelKonum).title("Esenler"))
+                mMap.moveCamera (CameraUpdateFactory.newLatLngZoom (guncelKonum, 15f) )
+            }
+
+        }
+
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            // izin verilmemiş
+            ActivityCompat.requestPermissions ( this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1)
+        }else{
+            // izin verilmiş
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,1f, locationListener)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        if (requestCode == 1){
+            if (grantResults.size>0){
+                if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED){
+                    // izin verildi
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,1f, locationListener)
+
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
